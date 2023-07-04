e7a95800ad722215a96307e41545de5225f1d2f0
Weiner Oliveira <woliveira82@gmail.com>
2023-07-04 17:46:18 +0100

    feat: set different star sprites

--- game/star_system.gd
+++ game/star_system.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
+
+const RED_STAR = preload("res://assets/red.png")
+const BLUE_STAR = preload("res://assets/blue.png")
+const GRAY_STAR = preload("res://assets/gray.png")
+
+@onready var sprite := $Sprite2D
 @onready var timer := $Timer
 @onready var power_label := $Power
 
 var power: int = 0 : set = set_power
-var player: bool = false
+@export var player: bool = false
+
+
+func _ready():
+	if player:
+		sprite.texture = BLUE_STAR
 
 
 func set_power(new_power):
